5e35d46035bb804e7952dbf29e49ffee54a19c7f
Bloo <43817879+bloo404@users.noreply.github.com>
2023-01-05 18:55:33 +0200

    add

---
+++ Semester 2/Data structures and algorithms/Project 5 SortedBag linked representation dynamic allocation/SortedBagIterator.h
@@ -0,0 +1,24 @@
+#pragma once
+#include "SortedBag.h"
+#include <stack>
+class SortedBag;
+
+class SortedBagIterator
+{
+	friend class SortedBag;
+
+private:
+	const SortedBag& bag;
+    int current;
+    std::stack<int> stack;
+	explicit SortedBagIterator(const SortedBag& b);
+
+
+public:
+	TComp getCurrent();
+	bool valid();
+	void next();
+	void first();
+
+};
+
